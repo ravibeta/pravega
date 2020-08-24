3b3d00a7bd66f20a34514d0d8a790c10b2143476
Ravi Rajamani <ravi.rajamani@emc.com>
2020-08-24 11:32:31 -0700

    enable a test run
    Signed-off-by: Ravi Rajamani <ravi.rajamani@emc.com>

--- test/system/src/main/java/io/pravega/test/system/framework/Utils.java
+++ test/system/src/main/java/io/pravega/test/system/framework/Utils.java
@@ -213,7 +213,7 @@ public class Utils {
     }
 
     private static boolean isTLSEnabled() {
-        String tlsEnabled = Utils.getConfig("tlsEnabled", "false");
+        String tlsEnabled = Utils.getConfig("tlsEnabled", "true");
         return Boolean.valueOf(tlsEnabled);
     }
 }
